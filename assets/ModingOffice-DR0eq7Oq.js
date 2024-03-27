f3a1d35cdd479bdb01163770d925ab4c25d19589
정태윤 <wjdxodbs52@naver.com>
2024-03-27 14:18:50 +0900

    저장

---
+++ assets/ModingOffice-DR0eq7Oq.js
@@ -0,0 +1,1 @@
+import{j as i,N as s}from"./index-Cs6NFu2b.js";const n=()=>i.jsxs("div",{className:"movielist-container",children:[i.jsx("div",{children:"무딩오피스페이지 입니다."}),i.jsx(s,{to:"/movie/list",children:i.jsx("div",{children:"무딩리스트로 아동하기"})})]});export{n as default};
