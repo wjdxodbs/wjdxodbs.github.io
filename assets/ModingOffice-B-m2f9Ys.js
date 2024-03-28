79e61da6629d41eb9ec6d5e3e805e0e302307fd5
wjdxodbs <wjdxodbs52@naver.com>
2024-03-29 07:40:42 +0900

    저장

--- assets/ModingOffice-B-m2f9Ys.js
+++
@@ -1,1 +0,0 @@
-import{j as i,N as s}from"./index-BUx5xPjx.js";const n=()=>i.jsxs("div",{className:"movielist-container",children:[i.jsx("div",{children:"무딩오피스페이지 입니다."}),i.jsx(s,{to:"/movie/list",children:i.jsx("div",{children:"무딩리스트로 아동하기"})})]});export{n as default};
