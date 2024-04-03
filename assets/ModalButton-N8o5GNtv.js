d9bf56a96c3c82ca073044e778d3d4c2004a7a9b
정태윤 <wjdxodbs52@naver.com>
2024-04-04 05:56:33 +0900

    저장

---
+++ assets/ModalButton-N8o5GNtv.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DH76QrY1.js";const c=({func:t,type:o,title:e})=>{const n=o?"bg-red-600":"bg-white text-black";return r.jsx("button",{onClick:t,className:`flex-1 p-[1.5vh] rounded-lg ${n}`,children:e})};export{c as M};
---
+++ assets/NotFoundPage-CAzosytP.js
@@ -0,0 +1,1 @@
+import{v as e,j as o,L as r}from"./index-DH76QrY1.js";const n=()=>{const s=e();return console.log(s),o.jsxs("div",{className:"h-[100vh]",children:[o.jsx("div",{children:"NotFoundPage"}),o.jsx(r,{to:"/",children:"무딩 홈으로"})]})};export{n as default};
