7d5fdfb3f980f0519d5d84675a82531a11171a57
wjdxodbs <wjdxodbs52@naver.com>
2024-04-28 22:06:28 +0900

    저장

--- assets/NotificationPage-Q9tCd8Bo.js
+++ assets/NotificationPage-Q9tCd8Bo.js
@@ -1,5 +1,1 @@
-<<<<<<<< HEAD:assets/NotificationPage-Q9tCd8Bo.js
 import{j as t}from"./index-DecRDppK.js";import{H as e}from"./Header-CFoBTzSv.js";import"./iconBase-D1dR2TQs.js";const s=()=>t.jsxs("div",{className:"NotificationPage",children:[t.jsx(e,{centerText:"알림"}),t.jsx("div",{children:"알림페이지 입니다"})]});export{s as default};
-========
-import{j as t}from"./index-Bmgexmlp.js";import{H as e}from"./Header-B0qgUnD6.js";import"./iconBase-B4GNIsdF.js";const s=()=>t.jsxs("div",{className:"NotificationPage",children:[t.jsx(e,{centerText:"알림"}),t.jsx("div",{children:"알림페이지 입니다"})]});export{s as default};
->>>>>>>> 1c5bf723bd365d07b5ebf544771133592d667c73:assets/NotificationPage-BEEklCDf.js
--- assets/PrivateRoute-BoqfE6FJ.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as e}from"./index-Bmgexmlp.js";function a(){return t.jsx(e,{})}export{a as default};
