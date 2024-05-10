2f66b6ca4204b0ce96e1ee91b945f7a5fb9f927d
wjdxodbs <wjdxodbs52@naver.com>
2024-05-10 21:25:33 +0900

    저장

---
+++ assets/MusicDropBtn-OtqsWfDJ.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-7bpdFbqo.js";const n=({onClick:t,btnText:r})=>s.jsx("div",{className:"put-btn-div",children:s.jsx("div",{className:"put-btn",onClick:t,children:r})});export{n as M};
