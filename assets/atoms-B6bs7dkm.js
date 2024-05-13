227820da8581ad8619765a5cb12784c5bee99f02
wjdxodbs <wjdxodbs52@naver.com>
2024-05-13 23:26:01 +0900

    저장

--- assets/atoms-B6bs7dkm.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./index-CRA80s5I.js";import{r as s}from"./index-Co6n4StN.js";const{persistAtom:t}=s({key:"musicDropInfoStorage",storage:sessionStorage}),i=e({key:"selectMusic",default:{youtubeId:"",albumImage:"",artist:"",title:"",playTime:"",previewUrl:""},effects_UNSTABLE:[t]});export{i as s};
